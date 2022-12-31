8f7d1b3b48eadfa52ccb60515951a32b2a43f3cc
Mark Fuller <Mark_Fuller1@baylor.edu>
2022-12-30 18:41:28 -0800

    stepper + LCD + thermistor

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -1,22 +1,40 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 1500;
+const int stepsPerRevolution = 2048;
 
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
-  myStepper.setSpeed(20);
+  myStepper.setSpeed(15);
 
   Serial.begin(9600);
 }
+  // Serial.println("clockwise");
+
+  // myStepper.step(stepsPerRevolution);
+  // delay(500);
+
+  // Serial.println("counter");
+  // myStepper.step(-stepsPerRevolution);
+  // delay(500);
 
 void loop(){
-  Serial.println("clockwise");
+  long milli = millis();
+
+  long seconds = milli / 1000;
+  long minutes = seconds / 60;
+  long hours = minutes / 60;
+
+  // Serial.println("-----------");
+  // Serial.print("hours:");
+  // Serial.println(hours);
+  // Serial.print("mins:");
+  // Serial.println(minutes);
+  // Serial.print("seconds:");
+  // Serial.println(seconds);
+  // Serial.print("millis:");
+  // Serial.println(milli);
 
   myStepper.step(stepsPerRevolution);
-  delay(500);
-
-  Serial.println("counter");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
+  Serial.println("stepped");
 }
